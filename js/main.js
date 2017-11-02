9130ed1a455229fef59c357410b1ee73418e02ae
lukasedeeeer <lukasedeeeer@gmail.com>
2017-11-03 00:52:48 +0100

    Added cookie dialog for evetual new magic!

--- js/main.js
+++ js/main.js
@@ -1,6 +1,31 @@
 /* </> Lukas Eder */
 
 $(document).ready(function () {
+    if (document.cookie == "") {
+        $('.cookie').css('display','block');
+    } else {
+        $('.cookie').remove();
+    }
+
+    function writeCookie (key, value, days) {
+        var date = new Date();
+        days = days || 365;
+        date.setTime(+ date + (days * 86400000));
+        window.document.cookie = key + "=" + value + "; expires=" + date.toGMTString() + "; path=/";
+        return value;
+    };
+
+    $('.cookieAccept').click(function () {
+        $('.cookie').remove();
+        writeCookie("cookieAccepted", "1", 365);
+    });
+
+    $('.cookieDeny').click(function () {
+        $('.cookie').remove();
+    });
+
+    // Sidebar
+
     $('.link-sidebar-open').click(function () {
         $('.link-sidebar').animate({left: "0"});
     });
