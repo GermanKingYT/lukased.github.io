53584219853964b47acf5b4464f955746e38bacc
lukasedeeeer <lukasedeeeer@gmail.com>
2017-11-05 18:20:30 +0100

    Changed some magic!

--- js/main.js
+++ js/main.js
@@ -13,11 +13,11 @@ $(document).ready(function () {
         date.setTime(+ date + (days * 86400000));
         window.document.cookie = key + "=" + value + "; expires=" + date.toGMTString() + "; path=/";
         return value;
-    };
+    }
 
     $('.cookieAccept').click(function () {
         $('.cookie').remove();
-        writeCookie("cookieAccepted", "1", 365);
+        writeCookie("cookieAccepted", true, 365);
     });
 
     $('.cookieDeny').click(function () {
